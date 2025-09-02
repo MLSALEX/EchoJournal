a9068fb5155b943b92ef0e98fa6debe6384a78c1
MLSALEX <stiopovna@mail.ru>
2025-09-02 10:11:27 +0300

    implement confirm leave dialog

--- app/src/main/java/com/alexmls/echojournal/app/navigation/Navigation.kt
+++ app/src/main/java/com/alexmls/echojournal/app/navigation/Navigation.kt
@@ -24,7 +24,7 @@ fun Navigation(
             )
         }
         composable<NavigationRoute.CreateEcho> {
-           CreateEchoRoot()
+           CreateEchoRoot(onConfirmLeave = navController::navigateUp)
         }
     }
 }
